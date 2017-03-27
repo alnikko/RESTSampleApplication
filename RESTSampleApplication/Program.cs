2a379657aeed8f13ceb85875a95c52ba2ad2ac06
alnikko <nikkgeek@outlook.com>
2017-03-27 12:22:12 +0800

    Initial Commit

--- RESTSampleApplication/Program.cs
+++ RESTSampleApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RESTSampleApplication.RESTWebService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,74 @@ using System.Threading.Tasks;
 
 namespace RESTSampleApplication
 {
+    /// <summary>
+    /// This is a sample of REST integration using Acumatica ERP
+    /// This is for educationla purpose only to study if the code is also applicable
+    /// in other ERP Application like Microsoft Dynamics 365
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int option;
+            Console.WriteLine("Select an option you want to do:");
+            Console.WriteLine("1. Create Customer");
+            Console.WriteLine("2. Get Customer Info");
+            Console.WriteLine("3. Create Sales Invoice");
+            Console.WriteLine("4. Create Invoice to Payments");
+
+            Console.Write("Enter option: ");
+            int.TryParse(Console.ReadLine(), out option);
+
+            switch (option)
+            {
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    Console.WriteLine("Value entered not in options.");
+                    Console.WriteLine(System.Environment.NewLine);
+                    Main();
+                    break;
+            }
+        }
+
+        void CreateCustomer(string customerID, string customerName, string email, string address1, string address2, string City)
+        {
+            using (DefaultEndpoint restClient = new DefaultEndpoint())
+            {
+                WebServiceConnector.InitializeWebService(restClient);
+
+                try
+                {
+                    Customer newCust = new Customer
+                    {
+                        CustomerID = new StringValue { Value = customerID },
+                        CustomerName = new StringValue { Value = customerName },
+                        MainContact = new Contact
+                        {
+                            Email = new StringValue { Value = email },
+                            Address = new Address
+                            {
+                                AddressLine1 = new StringValue { Value = address1 },
+                                AddressLine2 = new StringValue { Value = address2 },
+                                City = new StringValue { Value = City }
+                            }
+                        }
+                    };
+                }
+                finally
+                {
+                    restClient.Logout();
+                }
+            }
+
         }
     }
 }
---
+++ RESTSampleApplication/WebServiceConnector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RESTSampleApplication
+{
+    class WebServiceConnector
+    {
+        /// <summary>
+        /// Initializes Connection to webservice
+        /// </summary>
+        /// <param name="endpointClient">SOAP or REST Endpoint</param>
+        public static void InitializeWebService(RESTWebService.DefaultEndpoint endpointClient)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(ValidateRemoteCertificate);
+
+            //Endpoint login through SOAP Client
+            endpointClient.Login
+                (
+                    Properties.Settings.Default.Username,       // Username
+                    Properties.Settings.Default.Password,       // Password
+                    Properties.Settings.Default.CompanyName,    // Company name
+                    Properties.Settings.Default.Branch,         // Branch (if any),
+                    null                                        //locale (localization)
+                );
+        }
+        
+        /// <summary>
+        /// This is for checking if Website that will be accessing has an SSL Certificate
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="cert"></param>
+        /// <param name="chain"></param>
+        /// <param name="policyErrors"></param>
+        /// <returns></returns>
+        private static bool ValidateRemoteCertificate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors policyErrors)
+        {
+            //For simplicity, this callback always returns true
+            //In a real integration application, you must check
+            //an SSL certificate here
+            return true;
+        }
+    }
+}
